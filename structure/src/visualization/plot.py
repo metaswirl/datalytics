c401ddf2a03ac82d1f0208225f50a49bc703a74b
Niklas Semmler <nsemmler@inet.tu-berlin.de>
2019-03-12 15:46:08 +0100

    added new way to copy structure

---
+++ structure/src/visualization/plot.py
@@ -0,0 +1,18 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import datalytics as da
+import click
+
+@click.command()
+@click.argument('input_fpath', type=click.Path())
+@click.argument('output_fpath', type=click.Path())
+def main(input_fpath, output_fpath):
+    df = da.read_df(input_fpath)
+
+    f, ax = plt.subplots(1,1)
+
+    f.savefig(output_fpath)
+
+if __name__ == '__main__':
+    main()
---
+++ utils/init.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+#
+# init.sh
+# Copyright (C) 2019 Niklas Semmler <niklas.semmler@mailbox.org>
+#
+# Distributed under terms of the MIT license.
+#
+
+rsync -auv datalytics/structure ./
+
